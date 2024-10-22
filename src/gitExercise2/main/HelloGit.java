d18f62132cd00f1ef44774fb9662cbbff246d49e
Francesco Sormani <f.sormanicampus.unimib.it>
2024-10-22 15:13:19 +0200

    sono il primo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Questa e una stampa");
+		System.out.println("Questa e una nuovissima stampa");
 	}
 }
