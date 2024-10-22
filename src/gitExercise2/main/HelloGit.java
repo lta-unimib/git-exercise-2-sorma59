abfe61de65a325e1d38dc49aab6ab9f56990ea1f
Francesco Sormani <f.sormanicampus.unimib.it>
2024-10-22 15:24:24 +0200

    Scelta effettuata

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,10 +3,7 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-<<<<<<< HEAD
 		System.out.println("Questa e una nuovissima stampa");
-=======
-		System.out.println("Questa e una nuova stampa");
->>>>>>> bfd7614a6e4da6b40c7c139603b785da4a474ffb
+
 	}
 }
